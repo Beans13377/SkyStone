e9d4f960cc8e7a2da33fee2ec6c4b4a059b1fe51
Daniel Holleman <danielholleman20@gmail.com>
2019-11-16 16:12:03 -0600

    Create non-tank-drive teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SixMotorTeleOpNormalDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SixMotorTeleOpNormalDrive.java
@@ -3,25 +3,24 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp
-public class SixMotorTeleOp extends OpMode {
-    private static final double MAX_POWER = 1.0;
-    private static final double MAX_TURNING_POWER = 0.5;
+public class SixMotorTeleOpNormalDrive extends OpMode{
 
-    private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDriveMiddle = null;
     private DcMotor rightDriveMiddle = null;
     private DcMotor leftDriveFront = null;
     private DcMotor leftDriveBack = null;
     private DcMotor rightDriveFront = null;
     private DcMotor rightDriveBack = null;
+    private DcMotor intake = null;
+
+    private boolean lastPowerToggle = false;
+    private boolean isIntakePowerOn = false;
 
     @Override
     public void init() {
-        telemetry.addData("Status", "Initialized");
 
         leftDriveMiddle  = hardwareMap.get(DcMotor.class, "left_drive_middle");
         rightDriveMiddle = hardwareMap.get(DcMotor.class, "right_drive_middle");
@@ -29,60 +28,38 @@ public class SixMotorTeleOp extends OpMode {
         rightDriveFront = hardwareMap.get(DcMotor.class, "right_drive_front");
         leftDriveBack  = hardwareMap.get(DcMotor.class, "left_drive_back");
         rightDriveBack = hardwareMap.get(DcMotor.class, "right_drive_back");
+        intake = hardwareMap.get (DcMotor.class, "intake");
         leftDriveMiddle.setDirection(DcMotor.Direction.FORWARD);
         rightDriveMiddle.setDirection(DcMotor.Direction.REVERSE);
         leftDriveFront.setDirection(DcMotor.Direction.FORWARD);
         rightDriveFront.setDirection(DcMotor.Direction.REVERSE);
         leftDriveBack.setDirection(DcMotor.Direction.FORWARD);
         rightDriveBack.setDirection(DcMotor.Direction.REVERSE);
+        intake.setDirection(DcMotor.Direction.REVERSE);
+        leftDriveFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDriveBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDriveMiddle.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDriveFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDriveMiddle.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDriveBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         telemetry.addData("Status", "Initialized");
     }
-//
-//    @Override
-//    public void init_loop() {
-//    }
-//
-//    @Override
-//    public void start() {
-//        runtime.reset();
-//    }
 
     @Override
     public void loop() {
 
-        double leftPower;
-        double rightPower;
+        double drive = -gamepad1.left_stick_y;
+        double turn = gamepad1.right_stick_x;
 
-        leftPower = -gamepad1.left_stick_y;
-        rightPower  =  -gamepad1.right_stick_y;
-
-        if (leftPower == 0 && rightPower == 0) {
-            setDriveMotorPower(0,0);
-            telemetry.addData("Is Stopped", "True");
-        } else if ((leftPower > 0 && rightPower > 0) || (leftPower < 0 && rightPower < 0)) {
-            leftPower = calculateMotorPowerForDrivingStraight(leftPower);
-            rightPower = calculateMotorPowerForDrivingStraight(rightPower);
-            telemetry.addData("Is Turning", "False");
-        } else {
-            leftPower = calculateMotorPowerForTurning(leftPower);
-            rightPower = calculateMotorPowerForTurning(rightPower);
-            telemetry.addData("Is Turning", "True");
-        }
+        double leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+        double rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
         setDriveMotorPower(leftPower,rightPower);
 
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        processIntake();
     }
 
-    private double calculateMotorPowerForDrivingStraight (double Power) {
-        return Range.clip(Power  * MAX_POWER,-1.0,1.0);
-    }
-
-    private double calculateMotorPowerForTurning (double Power) {
-        return Range.clip(Power  * MAX_TURNING_POWER,-1.0,1.0);
-    }
 
     private void setDriveMotorPower (double leftPower, double rightPower) {
         leftDriveMiddle.setPower(leftPower);
@@ -92,8 +69,28 @@ public class SixMotorTeleOp extends OpMode {
         leftDriveBack.setPower(leftPower);
         rightDriveBack.setPower(rightPower);
     }
-//
-//    @Override
-//    public void stop() {
-//    }
+
+    private void processIntake () {
+        boolean current = gamepad1.b;
+        if (current == lastPowerToggle) {
+            return;
+        } else if (lastPowerToggle == false && current == true) {
+            toggleIntakePower ();
+        }
+        lastPowerToggle = current;
+
+        telemetry.addData("power toggle",lastPowerToggle);
+        telemetry.addData("Intake Motor Power",intake.getPower());
+    }
+
+    private void toggleIntakePower() {
+        if (isIntakePowerOn == true) {
+            isIntakePowerOn = false;
+            intake.setPower(0);
+        } else {
+            isIntakePowerOn = true;
+            intake.setPower(1);
+        }
+    }
+
 }
