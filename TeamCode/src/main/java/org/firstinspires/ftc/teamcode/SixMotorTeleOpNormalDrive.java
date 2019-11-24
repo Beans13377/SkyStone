365c9b3919549b55e2b55b144ac394371b1372f4
Daniel Holleman <danielholleman20@gmail.com>
2019-11-23 22:03:14 -0600

    Working on Drive to Depot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SixMotorTeleOpNormalDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SixMotorTeleOpNormalDrive.java
@@ -3,11 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp
-public class SixMotorTeleOpNormalDrive extends OpMode{
+public class SixMotorTeleOpNormalDrive extends OpMode {
 
     private static final double intakePower = 1.0;
 
@@ -25,16 +24,24 @@ public class SixMotorTeleOpNormalDrive extends OpMode{
     private int intakeDirection = 1;
     private double currentIntakePower = 0;
 
+    private static final double countsPerMotorRev = 383.6;
+    private static final double wheelDiameter = 4;
+    private static final double countsPerInch = countsPerMotorRev / (wheelDiameter * 3.1415);
+
+    private boolean lastDriveToDepotToggle = false;
+    private boolean driveToDepotToggle = false;
+    private boolean driveToDepotLastValue = false;
+
     @Override
     public void init() {
 
-        leftDriveMiddle  = hardwareMap.get(DcMotor.class, "left_drive_middle");
+        leftDriveMiddle = hardwareMap.get(DcMotor.class, "left_drive_middle");
         rightDriveMiddle = hardwareMap.get(DcMotor.class, "right_drive_middle");
-        leftDriveFront  = hardwareMap.get(DcMotor.class, "left_drive_front");
+        leftDriveFront = hardwareMap.get(DcMotor.class, "left_drive_front");
         rightDriveFront = hardwareMap.get(DcMotor.class, "right_drive_front");
-        leftDriveBack  = hardwareMap.get(DcMotor.class, "left_drive_back");
+        leftDriveBack = hardwareMap.get(DcMotor.class, "left_drive_back");
         rightDriveBack = hardwareMap.get(DcMotor.class, "right_drive_back");
-        intake = hardwareMap.get (DcMotor.class, "intake");
+        intake = hardwareMap.get(DcMotor.class, "intake");
         leftDriveMiddle.setDirection(DcMotor.Direction.FORWARD);
         rightDriveMiddle.setDirection(DcMotor.Direction.REVERSE);
         leftDriveFront.setDirection(DcMotor.Direction.FORWARD);
@@ -59,23 +66,25 @@ public class SixMotorTeleOpNormalDrive extends OpMode{
         double drive = -gamepad1.left_stick_y;
         double turn = gamepad1.right_stick_x;
 
-        double leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        double rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        double leftPower = Range.clip(drive + turn, -1.0, 1.0);
+        double rightPower = Range.clip(drive - turn, -1.0, 1.0);
 
-        setDriveMotorPower(leftPower,rightPower);
+//        setDriveMotorPower(leftPower, rightPower);
 
         processIntake();
         intakeDirection();
         intake.setPower(currentIntakePower * intakeDirection);
 
+        driveToDepot();
+
         telemetry.addData("Is intake on", lastPowerToggle);
         telemetry.addData("Intake Motor Power", "%.2f", intake.getPower());
-        telemetry.addData("Is intake forward",lastDirectionToggle);
+        telemetry.addData("Is intake forward", lastDirectionToggle);
         telemetry.addData("Intake Motor Direction", intakeDirection);
     }
 
 
-    private void setDriveMotorPower (double leftPower, double rightPower) {
+    private void setDriveMotorPower(double leftPower, double rightPower) {
         leftDriveMiddle.setPower(leftPower);
         rightDriveMiddle.setPower(rightPower);
         leftDriveFront.setPower(leftPower);
@@ -84,19 +93,19 @@ public class SixMotorTeleOpNormalDrive extends OpMode{
         rightDriveBack.setPower(rightPower);
     }
 
-    private void processIntake () {
+    private void processIntake() {
         boolean current = gamepad1.b;
         if (current == lastPowerToggle) {
             return;
         } else if (lastPowerToggle == false && current == true) {
-            toggleIntakePower ();
+            toggleIntakePower();
         }
         lastPowerToggle = current;
 
     }
 
     private void toggleIntakePower() {
-        if (isIntakePowerOn == true) {
+        if (isIntakePowerOn) {
             isIntakePowerOn = false;
             currentIntakePower = 0;
         } else {
@@ -105,12 +114,12 @@ public class SixMotorTeleOpNormalDrive extends OpMode{
         }
     }
 
-    private void intakeDirection () {
+    private void intakeDirection() {
         boolean current = gamepad1.y;
         if (current == lastDirectionToggle) {
             return;
-        } else if (lastDirectionToggle == false && current == true) {
-            toggleIntakeDirection ();
+        } else if (!lastDirectionToggle && current) {
+            toggleIntakeDirection();
         }
         lastDirectionToggle = current;
 
@@ -118,9 +127,97 @@ public class SixMotorTeleOpNormalDrive extends OpMode{
 
     private void toggleIntakeDirection() {
         if (intakeDirection == 1) {
-             intakeDirection = -1;
+            intakeDirection = -1;
         } else {
             intakeDirection = 1;
         }
     }
+
+    private boolean driveToDepot() {
+        final double driveSpeed = 0.5;
+        final double turnSpeed = 0.5;
+
+        boolean current = gamepad1.x;
+        if (current == driveToDepotToggle) {
+
+        } else if (!driveToDepotToggle && current) {
+
+            leftDriveMiddle.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightDriveMiddle.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            leftDriveMiddle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightDriveMiddle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        }
+        driveToDepotToggle = current;
+
+
+        return encoderDrive(driveSpeed, 76, 76, 2.0);
+
+    }
+
+    public boolean encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+        boolean followingEncoders = false;
+
+        boolean current = gamepad1.x;
+        if (current == lastDriveToDepotToggle) {
+
+        } else if (!lastDriveToDepotToggle && current) {
+
+            newLeftTarget = leftDriveMiddle.getCurrentPosition() + (int)(leftInches * countsPerInch);
+            newRightTarget = rightDriveMiddle.getCurrentPosition() + (int) (rightInches * countsPerInch);
+            leftDriveMiddle.setTargetPosition(newLeftTarget);
+            rightDriveMiddle.setTargetPosition(newRightTarget);
+            leftDriveMiddle.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightDriveMiddle.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftDriveMiddle.setPower(Math.abs(speed));
+            rightDriveMiddle.setPower(Math.abs(speed));
+            leftDriveFront.setPower(Math.abs(speed));
+            rightDriveFront.setPower(Math.abs(speed));
+            leftDriveBack.setPower(Math.abs(speed));
+            rightDriveBack.setPower(Math.abs(speed));
+
+            double startTime = getRuntime();
+
+        }
+        lastDriveToDepotToggle = current;
+
+
+//        while (opModeIsActive() &&
+//                ((getRuntime() - startTime) < timeoutS) &&
+//                (leftDriveMiddle.isBusy() && rightDriveMiddle.isBusy())){
+
+        // driveToDepotCurrent == false if encoder drive is done
+        boolean driveToDepotCurrent = !(!leftDriveMiddle.isBusy() && !rightDriveMiddle.isBusy());
+//        if (gamepad1.left_stick_x ||gamepad1.) {
+//            driveToDepotCurrent = true;
+//        }
+        if(driveToDepotCurrent == this.driveToDepotLastValue) {
+            if(driveToDepotCurrent) {
+                telemetry.addData("Path1",  "Running to %7d :%7d", leftDriveMiddle.getTargetPosition(),  rightDriveMiddle.getTargetPosition());
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                        leftDriveMiddle.getCurrentPosition(),
+                        rightDriveMiddle.getCurrentPosition());
+                telemetry.update();
+            }
+        }
+        else if (this.driveToDepotLastValue && !driveToDepotCurrent) {
+            leftDriveMiddle.setPower(0);
+            rightDriveMiddle.setPower(0);
+            leftDriveFront.setPower(0);
+            rightDriveFront.setPower(0);
+            leftDriveBack.setPower(0);
+            rightDriveBack.setPower(0);
+
+            leftDriveMiddle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightDriveMiddle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        }
+        this.driveToDepotLastValue = driveToDepotCurrent;
+    }
+
 }
